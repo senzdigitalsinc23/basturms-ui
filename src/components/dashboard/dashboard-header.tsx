82342c1805747274df75f69da5a631b2f11e8163
Douglas Senzu <senzu.dogi23@gmail.com>
2025-09-20 01:37:56 +0000

    remove the search bar in the header section

--- src/components/dashboard/dashboard-header.tsx
+++ src/components/dashboard/dashboard-header.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { UserNav } from './user-nav';
-import { Bell, Search } from 'lucide-react';
+import { Bell } from 'lucide-react';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 
@@ -10,14 +10,6 @@ export function DashboardHeader() {
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 sm:px-6">
       <div className="flex items-center gap-2 md:hidden">
         <SidebarTrigger />
-      </div>
-      <div className="relative flex-1">
-        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-        <Input
-          type="search"
-          placeholder="Search students..."
-          className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
-        />
       </div>
       <div className="flex flex-1 items-center justify-end gap-2">
         <Button variant="ghost" size="icon" className="rounded-full relative">
