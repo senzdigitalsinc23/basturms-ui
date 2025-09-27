0b8b71f3408effcd534ab07a4a6250746a580edb
Douglas Senzu <senzu.dogi23@gmail.com>
2025-09-27 05:06:41 +0000

    apply this date picker format on the manage terms form

--- src/components/settings/academic-settings.tsx
+++ src/components/settings/academic-settings.tsx
@@ -297,7 +297,7 @@ export function AcademicSettings() {
                                                     <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                                                 </Button></FormControl>
                                             </PopoverTrigger><PopoverContent className="w-auto p-0" align="start">
-                                                <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
+                                                <Calendar mode="single" selected={field.value} onSelect={field.onChange} captionLayout="dropdown-buttons" fromYear={new Date().getFullYear() - 5} toYear={new Date().getFullYear() + 5} initialFocus />
                                             </PopoverContent></Popover><FormMessage /></FormItem>
                                         )} />
                                         <FormField control={form.control} name={`terms.${index}.endDate`} render={({ field }) => (
@@ -308,7 +308,7 @@ export function AcademicSettings() {
                                                     <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                                                 </Button></FormControl>
                                             </PopoverTrigger><PopoverContent className="w-auto p-0" align="start">
-                                                <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
+                                                <Calendar mode="single" selected={field.value} onSelect={field.onChange} captionLayout="dropdown-buttons" fromYear={new Date().getFullYear() - 5} toYear={new Date().getFullYear() + 5} initialFocus />
                                             </PopoverContent></Popover><FormMessage /></FormItem>
                                         )} />
                                      </div>
@@ -362,7 +362,3 @@ export function AcademicSettings() {
         </div>
     )
 }
-
-    
-    
-  
