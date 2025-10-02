50122cf436a0c5cc1017f9b747d90191aa9bee9b
Douglas Senzu <senzu.dogi23@gmail.com>
2025-10-02 15:36:06 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/(dashboard)/attendance/my-classes/page.tsx
+++ src/app/(dashboard)/attendance/my-classes/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 import { ProtectedRoute } from '@/components/protected-route';
 import { ClassAttendanceTracker } from '@/components/student-management/attendance/class-attendance-tracker';
